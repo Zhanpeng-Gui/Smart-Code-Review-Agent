a3e82a14202820856f9341dba01081fc2d617732
Zhanpeng-Gui <guizhanpeng9@gmail.com>
2026-07-31 22:07:08 +0800

    Day2: integrate pylint static checker

---
+++ analyzer/pylint_checker.py
@@ -0,0 +1,61 @@
+# =========================================
+# pylint代码检查模块
+# Day2最终版
+#
+# 功能：
+# 接收Python代码
+# 调用pylint检查
+# 返回检查报告
+# =========================================
+
+
+# 文件路径处理
+import os
+
+
+# 调用系统命令
+import subprocess
+
+
+
+# 定义代码检查函数
+def check_python_code(code):
+
+
+    # 创建临时代码文件
+    file_path = "temp_code.py"
+
+
+
+    # 写入用户代码
+    with open(
+        file_path,
+        "w",
+        encoding="utf-8"
+    ) as file:
+
+        file.write(code)
+
+
+
+    # 调用pylint
+    result = subprocess.run(
+        [
+            "pylint",
+            file_path
+        ],
+
+        # 获取输出
+        capture_output=True,
+
+        # 转换文本
+        text=True
+    )
+
+
+    # 删除临时文件
+    os.remove(file_path)
+
+
+    # 返回检查结果
+    return result.stdout + result.stderr
--- main.py
+++ main.py
@@ -1,34 +1,65 @@
-#=======================================
-#智能代码评审Agent
-#Day 1
+# ======================================
+# 智能代码评审Agent
+# Day2版本
 #
-#功能：
-#1. 接受用户输入代码
-#2. 保存代码内容
-#3. 输出收到的代码
-#=======================================
+# 功能：
+# 1. 接收用户代码
+# 2. 调用pylint检查
+# 3. 输出代码问题
+# ======================================
 
 
-#定义一个函数
-#作用：接收用户输入的代码
+# 导入我们的检查模块
+from analyzer.pylint_checker import check_python_code
+
+
+
+# 接收用户代码
 def receive_code():
-    #提示用户输入代码
-    print("请输入代码（输入 'END' 结束）：")
+
+    print("请输入你的代码")
+    print("输入 END 表示结束")
+
+
     code_lines = []
+
+
     while True:
+
         line = input()
+
+
         if line == "END":
+
             break
+
+
         code_lines.append(line)
+
+
     code = "\n".join(code_lines)
+
+
     return code
 
-#Python程序入口
+
+
+# 程序入口
 if __name__ == "__main__":
-    #调用函数接收代码
+
+
+    # 获取用户代码
     user_code = receive_code()
-    #输出收到的代码
-    print("\n========= Agent收到的代码 =========\n")
-    print(user_code)
-    print("\n========= 分析结束 =========\n")
-    
+
+
+    print("\n========== 开始代码检查 ==========\n")
+
+
+    # 调用pylint
+    result = check_python_code(user_code)
+
+
+    print(result)
+
+
+    print("\n========== 检查结束 ==========")
