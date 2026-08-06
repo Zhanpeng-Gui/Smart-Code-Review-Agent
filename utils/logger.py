441c098d6514639b6cbde2a2718e5b7873e08287
Zhanpeng-Gui <guizhanpeng9@gmail.com>
2026-08-06 10:44:53 +0800

    Day8: improve logging system and ignore logs

--- utils/logger.py
+++ utils/logger.py
@@ -1,25 +1,66 @@
+"""
+日志模块
+
+作用：
+统一管理项目日志
+"""
+
 import logging
 import os
 
 
-if not os.path.exists("logs"):
-    os.makedirs("logs")
-
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    handlers=[
-        logging.FileHandler(
-            "logs/app.log",
-            encoding="utf-8"
-        ),
-        logging.StreamHandler()
-    ]
+# 获取项目根目录
+BASE_DIR = os.path.dirname(
+    os.path.dirname(
+        os.path.abspath(__file__)
+    )
 )
 
 
-# 屏蔽第三方库日志
+# 日志目录
+LOG_DIR = os.path.join(
+    BASE_DIR,
+    "logs"
+)
+
+
+# 不存在则创建
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
+
+
+LOG_FILE = os.path.join(
+    LOG_DIR,
+    "app.log"
+)
+
+
+
+logging.basicConfig(
+
+    level=logging.INFO,
+
+    format=(
+        "%(asctime)s "
+        "- %(levelname)s "
+        "- %(message)s"
+    ),
+
+    handlers=[
+
+        logging.FileHandler(
+            LOG_FILE,
+            encoding="utf-8"
+        ),
+
+        logging.StreamHandler()
+
+    ]
+
+)
+
+
+# 关闭第三方库日志
 logging.getLogger("httpx").setLevel(
     logging.WARNING
 )
